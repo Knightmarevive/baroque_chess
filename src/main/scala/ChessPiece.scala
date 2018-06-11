26ced91dcf8b3b06c38d21a1f297848fa6b247f3
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-11 21:57:12 +0200

    checkboard in middle state +++

--- src/main/scala/ChessPiece.scala
+++ src/main/scala/ChessPiece.scala
@@ -1,8 +1,8 @@
-import scala.Console.{GREEN_B,RED_B,BLACK,BLACK_B,RESET,BOLD}
+import scala.Console.{GREEN_B,RED_B,BLACK,BLACK_B,RESET,BOLD,YELLOW,BLUE}
 
 case class ChessPiece(side:Int, kind:String){
-  def ChessToString:String = { s"$RESET"+s"$BLACK"+s"$BOLD"+(
-    if(side==1) s"$GREEN_B" else (if (side==2) s"$RED_B" else s"$BLACK_B"+ s"$BLACK")
+  def ChessToString:String = { s"$RESET"+s"$BOLD"+(
+    if(side==1) s"$GREEN_B"+s"$YELLOW" else (if (side==2) s"$RED_B" + s"$BLUE" else s"$BLACK_B"+ s"$BLACK")
   )+kind+s"$RESET"}
 }
 
