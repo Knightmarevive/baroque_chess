55575ba2970a7904de705f7e3c8fafc0c68c2c44
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-11 14:03:59 +0200

    ChessPiece

--- src/main/scala/ChessPiece.scala
+++ src/main/scala/ChessPiece.scala
@@ -1,1 +1,7 @@
-case class ChessPiece(side:Int, kind:String)
+import scala.Console.{GREEN,RED,BLACK,RESET}
+
+case class ChessPiece(side:Int, kind:String){
+  def printme:Unit = print( s"$RESET"+(
+    if(side==1) s"$GREEN" else (if (side==2) s"$RED" else s"$BLACK")
+  )+kind+s"$RESET")
+}
