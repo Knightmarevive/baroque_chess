fbac1d1296791677e4a7b261b4993189c6ed048c
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-11 19:15:53 +0200

    starting checkboard

--- src/main/scala/CheckBoard.scala
+++ src/main/scala/CheckBoard.scala
@@ -1,4 +1,8 @@
 class CheckBoard (val fields: Vector[ChessPiece]) {
-
+  val lineBreaker: String = "-+-+-+-+-+-+-+-+-"
+  val columnMark: Vector[String] = Vector.empty[String] :+
+    " " :+ "1" :+ "2" :+ "3" :+ "4" :+ "5" :+ "6" :+ "7" :+ "8" :+ " "
+  val rowMark: Vector[String] = Vector.empty[String] :+
+    " " :+ "H" :+ "G" :+ "F" :+ "E" :+ "D" :+ "C" :+ "B" :+ "A" :+ " "
 
 }
