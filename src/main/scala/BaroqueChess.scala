30c44eb015b2ebe381d96a53d5a83a4894142bd6
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-11 21:45:40 +0200

    checkboard in middle state ++

--- src/main/scala/BaroqueChess.scala
+++ src/main/scala/BaroqueChess.scala
@@ -1,13 +1,7 @@
 object BaroqueChess {
   def main(args: Array[String]) {
-    //println("Hello, world!")
-    //val p1 : ChessPiece= ChessPiece(1,"K")
-    //val p2 : ChessPiece= ChessPiece(2,"D")
-    //val p3 : ChessPiece= ChessPiece(0,"+")
-    //p1.printme
-    //p2.printme
-    //p3.printme
-    val chk:CheckBoard = CheckBoard.createEmpty
+    //val chk:CheckBoard = CheckBoard.createEmpty
+    val chk:CheckBoard = CheckBoard.createStart
     chk.printme
     println("")
   }
