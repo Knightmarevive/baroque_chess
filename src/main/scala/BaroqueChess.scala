e37b438263afab62016d3a51dca8984936cfd1dd
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-12 21:41:54 +0200

    chess pieces beginning+  compass object  pincer move check++  MoveEffect++++  new file: doMove  first part of interaction +++

--- src/main/scala/BaroqueChess.scala
+++ src/main/scala/BaroqueChess.scala
@@ -2,14 +2,14 @@ object BaroqueChess {
   def main(args: Array[String]) {
     //val chk:CheckBoard = CheckBoard.createEmpty
     var chk:CheckBoard = CheckBoard.createStart
-    var side: Int = 1
+    var side: Int = 2
     while(true) {
       chk.printme
       val _move = TextToMove.ask
       val _ret = doMove.act(chk,_move._from.toInt,_move._to.toInt, side )
       if (_ret != None){
         println(" proper move ")
-        if (side != 1) side =1 else side = 2
+        if (side != 2) side =2 else side = 1
         chk=_ret.get
       }
     }
