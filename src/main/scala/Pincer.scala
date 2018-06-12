38a81eb79e820cf4ab910c161b769d075a61a09b
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-12 21:33:45 +0200

    chess pieces beginning+  compass object  pincer move check++  MoveEffect++++  new file: doMove  first part of interaction ++

--- src/main/scala/Pincer.scala
+++ src/main/scala/Pincer.scala
@@ -1,6 +1,7 @@
 object Pincer extends PieceMove {
   override def whichPiece: String = ChessPiece.Pincer
   override def fieldAvailible(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int) : Boolean ={
+    println(" trying to move Pincer")
     if( (placeTo<0 || placeTo>63) ||
       !(isThere(chk, placeFrom, side)|| CheckBoard.fieldsInLine(placeFrom,placeTo) )
         ) return false
