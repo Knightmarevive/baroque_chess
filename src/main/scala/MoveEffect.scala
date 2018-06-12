e9210506f4e6902f81eb41402c74e0a84a97c882
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-12 15:33:04 +0200

    chess pieces beginning+  compass object  pincer move check++  MoveEffect++++

--- src/main/scala/MoveEffect.scala
+++ src/main/scala/MoveEffect.scala
@@ -1,3 +1,14 @@
 case class MoveEffect(piece: ChessPiece, place: Int, remove: List[Int]) {
+  def +(that: Int): MoveEffect = {
+    MoveEffect(piece,place,remove :+ that)
+  }
+  def +(that: List[Int]): MoveEffect = {
+    MoveEffect(piece,place,remove ++ that)
+  }
+}
 
+object MoveEffect {
+  def moveWithoutKill(chk: CheckBoard, moveFrom: Int, moveTo: Int) : MoveEffect ={
+    MoveEffect(chk.fields(moveFrom),moveTo,List(moveTo))
+  }
 }
