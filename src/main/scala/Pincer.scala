b45f063d61173facd1c60a3adc5edbed27ae3c10
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-12 13:21:56 +0200

    chess pieces beginning+

--- src/main/scala/Pincer.scala
+++ src/main/scala/Pincer.scala
@@ -1,7 +1,7 @@
 object Pincer extends PieceMove {
   override def whichPiece: String = ChessPiece.Pincer
   override def fieldAvailible(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int) : Boolean ={
-    if(!(isThere(chk, placeFrom, side))| CheckBoard.fieldsInLine(placeFrom,placeTo) ) return false
+    if(!(isThere(chk, placeFrom, side)| CheckBoard.fieldsInLine(placeFrom,placeTo) ) ) return false
 
     false
   }
