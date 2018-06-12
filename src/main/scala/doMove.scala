ad2b9c0845160a2de355245d56a98d84c00e22e5
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-12 19:58:34 +0200

    chess pieces beginning+  compass object  pincer move check++  MoveEffect++++  new file: doMove

--- src/main/scala/doMove.scala
+++ src/main/scala/doMove.scala
@@ -3,5 +3,20 @@ class doMove {
 }
 
 object doMove {
+  def act(chk: CheckBoard, _from: Int, _to: Int, _side: Int): Option[CheckBoard] = {
+    if( (!chk.fields(_from).SameSide(_side ) ) || chk.fields(_to).SameSide(_side)) None else
+      {
+        val currentPiece = chk.fields(_from).kind
+        currentPiece match {
+          case ChessPiece.Pincer => {
+            if (Pincer.fieldAvailible(chk,_from,_to,_side))
+              Some(chk + MoveEffect.moveWithoutKill(chk,_from,_to))
+            else None
+          }
 
+          case _ => None
+
+        }
+      }
+  }
 }
