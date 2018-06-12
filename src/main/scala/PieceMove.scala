99240e48df4bf9a9f688d43e1a09aeb31a8774f1
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-12 15:19:29 +0200

    chess pieces beginning+ added compass object added pincer move check++ added MoveEffect++

--- src/main/scala/PieceMove.scala
+++ src/main/scala/PieceMove.scala
@@ -1,6 +1,7 @@
 trait PieceMove {
   def whichPiece: String
   def isThere(chk: CheckBoard, place: Int, side: Int) : Boolean = {
+    if (place<0 || place>63) false else
     chk.fields(place) == ChessPiece(side,whichPiece)
   }
   def fieldAvailible(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int) : Boolean
--- src/main/scala/Pincer.scala
+++ src/main/scala/Pincer.scala
@@ -1,7 +1,9 @@
 object Pincer extends PieceMove {
   override def whichPiece: String = ChessPiece.Pincer
   override def fieldAvailible(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int) : Boolean ={
-    if(!(isThere(chk, placeFrom, side)| CheckBoard.fieldsInLine(placeFrom,placeTo) ) ) return false
+    if( (placeTo<0 || placeTo>63) ||
+      !(isThere(chk, placeFrom, side)|| CheckBoard.fieldsInLine(placeFrom,placeTo) )
+        ) return false
     val cmp = Compass.direction(placeFrom,placeTo)
 
     for(i <- Range(placeFrom,cmp+placeTo,cmp.toInt))
