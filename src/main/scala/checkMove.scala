3ceea70ac9252e5134677e7aa86fe0839d19a269
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-13 19:56:31 +0200

    Todo: NegaScout AI

--- src/main/scala/checkMove.scala
+++ src/main/scala/checkMove.scala
@@ -1,10 +1,11 @@
 case class checkMove(_side :Int) {
     def allMoves(from :CheckBoard): List[CheckBoard] = {
-      for(i <- 0 to 63; if (from.fields(i).SameSide(_side)))
-        if(from.isInFear(i)) doMove.act(from,i,i,_side) else
-        for(j <- 0 to  63; if(((from.fields(i).kind == ChessPiece.King && CheckBoard.fieldsNear(i,j)) ||
+      (for(i <- (0 to 63).toList; if (from.fields(i).SameSide(_side))) yield
+        if(from.isInFear(i)) List[CheckBoard](doMove.act(from,i,i,_side).get) else
+        for(j <- (0 to 63).toList; if(((from.fields(i).kind == ChessPiece.King && CheckBoard.fieldsNear(i,j)) ||
           (from.fields(i).kind == ChessPiece.Pincer && CheckBoard.fieldsInLine(i,j)) ||
           (from.fields(i).kind != ChessPiece.King && from.fields(i).kind != ChessPiece.Pincer &&
-            CheckBoard.fieldsOnStar(i,j))) )) yield doMove.act(from,i,j,_side)
+            CheckBoard.fieldsOnStar(i,j))) && (doMove.act(from,i,j,_side).isDefined ) )) yield doMove.act(from,i,j,_side).get
+        ).flatten
     }
 }
