98275b464d0df53aa79b583d8e85eda6abb8dc95
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-13 16:02:46 +0200

    TODO: Dragon and Fear

--- src/main/scala/doMove.scala
+++ src/main/scala/doMove.scala
@@ -4,57 +4,59 @@ class doMove {
 
 object doMove {
   def act(chk: CheckBoard, _from: Int, _to: Int, _side: Int): Option[CheckBoard] = {
-    if( (!chk.fields(_from).SameSide(_side ) ) || chk.fields(_to).SameSide(_side)) None else
-      {
+    if(chk.fields(_from).SameSide(_side ) ) {
+      if (chk.fields(_to).SameSide(_side) || chk.isInFear(_from)) {
+        None
+      } else {
         val currentPiece = chk.fields(_from).kind
         //println(" trying to move ("+currentPiece+")")
         currentPiece match {
           case ChessPiece.Pincer => {
-            if (Pincer.fieldAvailible(chk,_from,_to,_side))
-              Some(chk + (MoveEffect.moveWithoutKill(chk,_from,_to)+Pincer.fieldsToBurn(
-                chk,_from,_to, _side
+            if (Pincer.fieldAvailible(chk, _from, _to, _side))
+              Some(chk + (MoveEffect.moveWithoutKill(chk, _from, _to) + Pincer.fieldsToBurn(
+                chk, _from, _to, _side
               )))
             else None
           }
-          case ChessPiece.King =>  {
-            if (King.fieldAvailible(chk,_from,_to,_side))
-              Some(chk + (MoveEffect.moveWithoutKill(chk,_from,_to)+King.fieldsToBurn(
-                chk,_from,_to, _side
+          case ChessPiece.King => {
+            if (King.fieldAvailible(chk, _from, _to, _side))
+              Some(chk + (MoveEffect.moveWithoutKill(chk, _from, _to) + King.fieldsToBurn(
+                chk, _from, _to, _side
               )))
             else None
           }
-          case ChessPiece.Withdrawer =>  {
-            if (Withdrawer.fieldAvailible(chk,_from,_to,_side))
-              Some(chk + (MoveEffect.moveWithoutKill(chk,_from,_to)+Withdrawer.fieldsToBurn(
-                chk,_from,_to, _side
+          case ChessPiece.Withdrawer => {
+            if (Withdrawer.fieldAvailible(chk, _from, _to, _side))
+              Some(chk + (MoveEffect.moveWithoutKill(chk, _from, _to) + Withdrawer.fieldsToBurn(
+                chk, _from, _to, _side
               )))
             else None
           }
-          case ChessPiece.Dragon=>  {
-            if (Dragon.fieldAvailible(chk,_from,_to,_side))
-              Some(chk + (MoveEffect.moveWithoutKill(chk,_from,_to)+Dragon.fieldsToBurn(
-                chk,_from,_to, _side
+          case ChessPiece.Dragon => {
+            if (Dragon.fieldAvailible(chk, _from, _to, _side))
+              Some(chk + (MoveEffect.moveWithoutKill(chk, _from, _to) + Dragon.fieldsToBurn(
+                chk, _from, _to, _side
               )))
             else None
           }
-          case ChessPiece.Coordinator=>  {
-            if (Coordinator.fieldAvailible(chk,_from,_to,_side))
-              Some(chk + (MoveEffect.moveWithoutKill(chk,_from,_to)+Coordinator.fieldsToBurn(
-                chk,_from,_to, _side
+          case ChessPiece.Coordinator => {
+            if (Coordinator.fieldAvailible(chk, _from, _to, _side))
+              Some(chk + (MoveEffect.moveWithoutKill(chk, _from, _to) + Coordinator.fieldsToBurn(
+                chk, _from, _to, _side
               )))
             else None
           }
-          case ChessPiece.LongLeaper=>  {
-            if (LongLeaper.fieldAvailible(chk,_from,_to,_side))
-              Some(chk + (MoveEffect.moveWithoutKill(chk,_from,_to)+LongLeaper.fieldsToBurn(
-                chk,_from,_to, _side
+          case ChessPiece.LongLeaper => {
+            if (LongLeaper.fieldAvailible(chk, _from, _to, _side))
+              Some(chk + (MoveEffect.moveWithoutKill(chk, _from, _to) + LongLeaper.fieldsToBurn(
+                chk, _from, _to, _side
               )))
             else None
           }
-          case ChessPiece.Imitator=>  {
-            if (Imitator.fieldAvailible(chk,_from,_to,_side))
-              Some(chk + (MoveEffect.moveWithoutKill(chk,_from,_to)+Imitator.fieldsToBurn(
-                chk,_from,_to, _side
+          case ChessPiece.Imitator => {
+            if (Imitator.fieldAvailible(chk, _from, _to, _side))
+              Some(chk + (MoveEffect.moveWithoutKill(chk, _from, _to) + Imitator.fieldsToBurn(
+                chk, _from, _to, _side
               )))
             else None
           }
@@ -63,5 +65,7 @@ object doMove {
 
         }
       }
+    } else None
+
   }
 }
