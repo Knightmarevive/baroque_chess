139dd10347c754e073752158bf40bc2199038380
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-13 20:26:56 +0200

    King cant enter checked position Todo: NegaScout AI

--- src/main/scala/King.scala
+++ src/main/scala/King.scala
@@ -2,8 +2,15 @@ object King extends PieceMove {
   override def whichPiece: String = ChessPiece.King
 
   override def ownValue: Long = 9999999
+
+  def fieldIsInCheck(chk: CheckBoard,placeFrom: Int, placeTo: Int,_side:Int): Boolean ={
+    val chkOpp = chk + MoveEffect.moveWithoutKill(chk,placeFrom,placeTo)
+    val tryOpp = checkMove(checkMove.Opponent(_side))
+    for (i <- tryOpp.allMoves(chkOpp)) if(i.findKing(_side)<0) return true
+    false
+  }
   override def fieldAvailible(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int): Boolean = {
-    CheckBoard.fieldsNear(placeFrom,placeTo)
+    CheckBoard.fieldsNear(placeFrom,placeTo) && (!fieldIsInCheck(chk,placeFrom: Int, placeTo: Int, side))
   }
 
   override def fieldsToBurn(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int): List[Int] = {
--- src/main/scala/checkMove.scala
+++ src/main/scala/checkMove.scala
@@ -1,12 +1,12 @@
 case class checkMove(_side :Int) {
-    def allMoves(from :CheckBoard): List[CheckBoard] = (if(_side==0) List[CheckBoard]() else ((
+    def allMoves(from :CheckBoard): List[CheckBoard] = if(_side==0) List[CheckBoard]() else (
       for(i <- (0 to 63).toList; if (from.fields(i).SameSide(_side))) yield
       if(from.isInFear(i)) List[CheckBoard](doMove.act(from,i,i,_side).get) else
       for(j <- (0 to 63).toList; if ((from.fields(i).kind == ChessPiece.King && CheckBoard.fieldsNear(i,j)) ||
         (from.fields(i).kind == ChessPiece.Pincer && CheckBoard.fieldsInLine(i,j)) ||
         (from.fields(i).kind != ChessPiece.King && from.fields(i).kind != ChessPiece.Pincer &&
           CheckBoard.fieldsOnStar(i,j))) && (doMove.act(from,i,j,_side).isDefined )) yield doMove.act(from,i,j,_side).get
-      )).flatten)
+      ).flatten
 
     def switch: checkMove = if(_side==1) checkMove(2) else if (_side==2) checkMove(1) else checkMove(0)
 }
@@ -14,4 +14,5 @@ case class checkMove(_side :Int) {
 object checkMove {
   val upper = checkMove(1)
   val lower = checkMove(2)
+  def Opponent(player :Int) :Int = if (player==1) 2 else  if (player==2) 1 else 0
 }
