55c169566eb3725d132a6d8d324c10d44f63dd95
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-13 15:13:10 +0200

    doMove LongLeaper Pincer King Withdrawer dragon(parts), Coordinator imitator (parts)

--- src/main/scala/Imitator.scala
+++ src/main/scala/Imitator.scala
@@ -1,3 +1,34 @@
-object Imitator {
+object Imitator extends PieceMove {
+    override def whichPiece: String = ChessPiece.Imitator
+
+
+  override def fieldAvailible(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int): Boolean = {
+    if(CheckBoard.fieldsNear(placeFrom,placeTo) &&
+      chk.fields(placeTo).kind==ChessPiece.King &&
+      chk.fields(placeTo).OpposeSide(side)) true else
+    if(chk.fields(placeTo).isEmpty){
+      val cmp = Compass.direction(placeFrom,placeTo)
+
+      for (i <- Range(cmp + placeFrom, placeTo, cmp.toInt))
+        if(chk.fields(i).SameSide(side) || chk.fields(i).kind != ChessPiece.LongLeaper ||
+          (chk.fields(i).OpposeSide(side) && !chk.fields(cmp+i).isEmpty)
+        ) return false
+
+      true
+    } else false
+  }
+
+  override def fieldsToBurn(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int): List[Int] = {
+    val cmp = Compass.direction(placeFrom,placeTo)
+
+    (for (i <- Range(cmp + placeFrom, placeTo, cmp.toInt).toList;
+         if (chk.fields(i).OpposeSide(side)))
+      yield i) ++ (if(CheckBoard.fieldsInLine(placeFrom,placeTo)) {
+      for (ppp <- Pincer.fieldsToBurn(chk,placeFrom,placeTo,side);
+           if (chk.fields(ppp).kind == ChessPiece.Pincer)
+      ) yield ppp
+    } else List[Int]())
+
+  }
 
 }
