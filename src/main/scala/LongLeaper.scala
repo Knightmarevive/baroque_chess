c6714f0b78f4c517ace1d2428645404fb68820fe
Maciej Czerniawski <maciek.czerniawski@gmail.com>
2018-06-13 14:46:40 +0200

    doMove LongLeaper Pincer King Withdrawer dragon(parts), Coordinator

--- src/main/scala/LongLeaper.scala
+++ src/main/scala/LongLeaper.scala
@@ -1,3 +1,26 @@
-object LongLeaper {
+object LongLeaper extends PieceMove {
+  override def whichPiece: String = ChessPiece.LongLeaper
+
+  override def fieldAvailible(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int): Boolean = {
+    if(chk.fields(placeTo).isEmpty){
+      val cmp = Compass.direction(placeFrom,placeTo)
+
+      for (i <- Range(cmp + placeFrom, placeTo, cmp.toInt))
+        if(chk.fields(i).SameSide(side) ||
+          (chk.fields(i).OpposeSide(side) && !chk.fields(cmp+i).isEmpty)
+        ) return false
+
+           true
+    } else false
+  }
+
+  override def fieldsToBurn(chk: CheckBoard, placeFrom: Int, placeTo: Int, side: Int): List[Int] = {
+    val cmp = Compass.direction(placeFrom,placeTo)
+
+    for (i <- Range(cmp + placeFrom, placeTo, cmp.toInt).toList;
+       if (chk.fields(i).OpposeSide(side)))
+         yield i
+
+  }
 
 }
